2f20b0ce84d0c88d15ef241fc14a612b31e5507a
Ryan Alameddine <rhalameddine@gmail.com>
2018-08-12 10:47:19 -0700

    Loads champ mastery flag and splash art

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,7 +1,7 @@
 function getRequest(link, callback)
 {
     let request = new XMLHttpRequest();
-    request.open("GET", "https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com" + link + "?api_key=RGAPI-fb50e5c7-3b85-4ab1-bdac-f11474f9068e");
+    request.open("GET", "https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com" + link + "?api_key=RGAPI-e25cb698-c710-4f88-902b-7d12b0216e4a");
     request.setRequestHeader("Content-Type", "application/json");
     request.onreadystatechange = () =>{
         if(request.readyState == 4 && request.status >= 200 && request.status < 300){
--- Scripts/summoner.js
+++ Scripts/summoner.js
@@ -16,16 +16,42 @@ class Summoner{
 }
 
 function loadPage(name){
+    //load summoner info
     let summoner = new Summoner(name, () => {
+        //set summoner name
         document.getElementById("summonerName").textContent = name;
+        //get version
         getDragonRequest("/api/versions.json", (versions) => {
             let version = versions[0];
-            document.getElementById("summonerIcon").setAttribute("src", "http://ddragon.leagueoflegends.com/cdn/" + version + "/img/profileicon/" + summoner.profileIconId + ".png");
-            getRequest("/lol/champion-mastery/v3/champion-masteries/by-summoner/" + summoner.id, (champMasteries) =>{
+            //set summoner icon
+            document.getElementById("summonerIcon").setAttribute("src", `http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${summoner.profileIconId}.png`);
+            //get champion masteries
+            getRequest(`/lol/champion-mastery/v3/champion-masteries/by-summoner/${summoner.id}`, (champMasteries) =>{
+                //get champion with highest mastery
                 let bestChamp = champMasteries[0];
                 let champId = bestChamp.championId;
-                getRequest("/lol/platform/v3/champions/" + champId, ())
-                getDragonRequest("/cdn/img/champion/splash/" + )
+                //set champion mastery flag
+                let masteryLevel = bestChamp.championLevel;
+                document.getElementById("championMastery").setAttribute("src", `https://github.com/RiotAPI/Riot-Games-API-Developer-Assets/raw/master/champion-mastery-icons/mastery-${masteryLevel}.png`);
+                //get DDragon champion json
+                getDragonRequest(`/cdn/${version}/data/en_US/champion.json`, (championJson) => {
+                    //get champion name
+                    let data = championJson.data;
+                    let champName;
+                    for (let champObj in data) {
+                        if(data[champObj].key == champId){
+                            champName = champObj;
+                            break;
+                        }
+                    }
+                    //get champion specific data from DDragon
+                    getDragonRequest(`/cdn/${version}/data/en_US/champion/${champName}.json`, (champData) => {
+                        //get random skin of champ
+                        let skinCount = champData.data[champName].skins.length;
+                        document.getElementById("championSplash").setAttribute("src", `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champName}_${champData.data[champName].skins[Math.floor(Math.random() * skinCount)].num}.jpg`);
+                        
+                    });
+                });
             });
         });
     });
