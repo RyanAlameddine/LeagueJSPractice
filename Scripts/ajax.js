3a1bc74acddddfa91f9a97051e23c53b7e2aec06
Ryan Alameddine <rhalameddine@gmail.com>
2018-09-09 12:30:49 -0700

    Fixed huge bug with match loading and began formatting for loading screen

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,7 +1,7 @@
 function getRequest(link, callback)
 {
     let request = new XMLHttpRequest();
-    request.open("GET", "https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com" + link + "?api_key=RGAPI-fa6d3ec0-efae-456b-9f99-fb7cfecb67a6");
+    request.open("GET", "https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com" + link + "?api_key=RGAPI-f81b7561-3333-4af9-aa35-6bfa255759c9");
     request.setRequestHeader("Content-Type", "application/json");
     request.onreadystatechange = () =>{
         if(request.readyState == 4 && request.status >= 200 && request.status < 300){
--- Scripts/match.js
+++ Scripts/match.js
@@ -1,12 +1,42 @@
 class Match{
+    
     constructor(response){
-        
+        this.bannedChampions = response.bannedChampions;
+        this.gameId = response.gameId;
+        this.participants = response.participants;
     }
 }
 
 window.onload = function(){
     id = getParamFromUrl('id');
-    getRequest("/lol/spectator/v3/active-games/by-summoner/" + id, (response) => {
-        match = new Match(response);
-    });      
+    getDragonRequest("/api/versions.json", (versions) => {
+        let version = versions[0];
+        getDragonRequest(`/cdn/${version}/data/en_US/champion.json`, (championJson) => {
+            getRequest("/lol/spectator/v3/active-games/by-summoner/" + id, (response) => {
+                match = new Match(response);
+                team100 = 0;
+                team200 = 0;
+                for(participant of response.participants){
+                    champName = getChampName(participant.championId, championJson);
+                    if(participant.teamId === 100){
+                        this.document.getElementById(`champ100-${team100}`).setAttribute("src", `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champName}_0.jpg`);
+                        team100++;
+                    }else{
+                        this.document.getElementById(`champ200-${team200}`).setAttribute("src", `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champName}_0.jpg`);
+                        team200++;
+                    }
+                }
+            });
+        });
+    });
+          
+}
+
+function getChampName(id, championJson){
+    let data = championJson.data;
+    for (let champObj in data) {
+        if(data[champObj].key == id){
+            return champObj;
+        }
+    }
 }
